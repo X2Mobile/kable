89fe34235f1efbe15a38a2c56b8f4ebb52e0992d
robertbakk <robertbakk@yahoo.com>
2025-09-24 19:50:31 +0300

    Update kable

--- kable-core/src/jvmMain/kotlin/com/juul/kable/Bluetooth.kt
+++ kable-core/src/jvmMain/kotlin/com/juul/kable/Bluetooth.kt
@@ -12,4 +12,4 @@ public actual class Device
 // This is not a proper implementation, but this property is deprecated, so...
 internal actual val bluetoothAvailability: Flow<Bluetooth.Availability> = flowOf(Bluetooth.Availability.Available)
 
-internal actual val bluetoothBondedDevices: Flow<List<Device>> = jvmNotImplementedException()
+internal actual val bluetoothBondedDevices: Flow<List<Device>> = flowOf()
